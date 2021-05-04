550ffc398862cefce1d3c999c0a18b2942e15f45
napalmtorch <napalmtorch@hotmail.com>
2021-05-04 16:35:37 -0400

    poop

--- src/core/kernel.c
+++ src/core/kernel.c
@@ -2,3 +2,15 @@ void kernel_main(void)
 {
     
 }
+//this?
+// yeah this will work - okkk
+
+//WHERE IS IT?
+
+// the git project? the folder i just created on your desktop
+
+// 2 monitors lmfao - YAYY
+
+//now to push
+
+// poop
