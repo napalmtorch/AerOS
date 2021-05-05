e7752105f561bdd94418a751ff4061d22db5bc62
napalmtorch <napalmtorch@hotmail.com>
2021-05-05 17:18:01 -0400

    PCI

--- src/hardware/drivers/pci.cpp
+++ src/hardware/drivers/pci.cpp
@@ -1,6 +1,9 @@
 #include "hardware/drivers/pci.hpp"
 #include "core/kernel.hpp"
 
+#define PCI_CONFIG_ADDRESS 0xCF8
+#define PCI_CONFIG_DATA    0xCFC
+
 namespace HAL
 {
     // initialize pci bus controller driver
@@ -50,41 +53,144 @@ namespace HAL
 
     void PCIBusController::AddDevice(PCIDevice* device)
     {
-
-    }
-
-    char* PCIBusController::GetVendorName(uint16_t vendor, uint16_t id)
-    {
-        
+        Devices[DeviceCount] = device;
+        DeviceCount++;
     }
     
     uint16_t PCIBusController::ReadWord(uint16_t bus, uint16_t slot, uint16_t func, uint16_t offset)
     {
-
+        uint32_t address;
+        uint32_t lbus = (uint32_t)bus;
+        uint32_t lslot = (uint32_t)slot;
+        uint32_t lfunc = (uint32_t)func;
+        uint16_t temp = 0;
+        address = (uint32_t)((lbus << 16) | (lslot << 11) | (lfunc << 8) | (offset & 0xFC) | ((uint32_t)0x80000000));
+        outl(PCI_CONFIG_ADDRESS, address);
+        temp = (uint16_t)((inl(PCI_CONFIG_DATA) >> ((offset & 2) * 8)) & 0xFFFF);
+        return (temp);
     }
 
     void PCIBusController::WriteWord(uint16_t bus, uint16_t slot, uint16_t func, uint16_t offset, uint16_t data)
     {
-    
+        uint32_t address = (uint32_t)((bus << 16) | (slot << 1) | (func << 8) | (offset & 0xFC) | ((uint32_t)0x80000000));
+        outl(PCI_CONFIG_ADDRESS, address);
+        outl(PCI_CONFIG_DATA, data);
     }
 
-    uint16_t PCIBusController::GetVendorID(uint16_t bus, uint16_t device, uint16_t func)
+    uint16_t PCIBusController::GetVendorID(uint16_t bus, uint16_t device, uint16_t function)
     {
-
+        uint16_t r0 = ReadWord(bus, device, function, 0x0);
+        return r0;
     }
 
     uint16_t PCIBusController::GetDeviceID(uint16_t bus, uint16_t device, uint16_t function)
     {
-
+        uint32_t r0 = ReadWord(bus, device, function, 0x2);
+        return r0;
     }
 
     uint16_t PCIBusController::GetClassID(uint16_t bus, uint16_t device, uint16_t function)
     {
-
+        uint32_t r0 = ReadWord(bus, device, function, 0xA);
+        return r0;
     }
 
     uint16_t PCIBusController::GetSubClassID(uint16_t bus, uint16_t device, uint16_t function)
     {
+        uint32_t r0 = ReadWord(bus, device, function, 0xA);
+        return (r0 & ~0xFF00);
+    }
 
+    char* PCIBusController::GetVendorName(uint16_t vendor, uint16_t id)
+    {
+        // intel devices
+        if (vendor == PCIVendors::Intel)
+        {
+            switch (id)
+            {
+                case 0x1237: { return "Intel 440FX Chipset"; }
+                case 0x7000: { return "Intel 82371SB PIIX3 ISA"; }
+                case 0x7010: { return "Intel 82371SB PIIX3 IDE"; }
+                case 0x7111: { return "Intel 82371AB/EB/MB PIIX4 IDE"; }
+                case 0x7113: { return "Intel 82371AB/EB/MB PIIX4 ACPI"; }
+                case 0x100E: { return "Intel 82540EM Gigabit Ethernet Controller"; }
+                case 0x0041: { return "Intel Core Processor PCI Express x16 Root Port"; }
+                case 0x0042: { return "Intel Core Processor IGPU Controller"; }
+                case 0x0044: { return "Intel Core Processor DRAM Controller"; }
+                case 0x0600: { return "Intel RAID Controller"; }
+                case 0x061F: { return "Intel 80303 I/O Processor"; }
+                default: { return "Unrecognized Intel Device"; }
+            }
+        }
+
+        // newer intel devices
+        if (vendor == PCIVendors::IntelCorp)
+        {
+            switch (id)
+            {
+                default: { return "Unrecognized Intel Device"; }
+            }
+        }
+
+        // vmware
+        if (vendor == PCIVendors::VMWare)
+        {
+            switch (id)
+            {
+                case 0x0405: { return "VMWare SVGAII Adapter"; }
+                case 0x0710: { return "VMWare SVGA Adapter"; }
+                case 0x0770: { return "VMWare USB2 EHCI Controller"; }
+                case 0x0720: { return "VMWare VMXNET Ethernet Controller"; }
+                case 0x0740: { return "VMWare VM Communication Interface"; }
+                case 0x0774: { return "VMWare USB1.1 UHCI Controller"; }
+                case 0x0778: { return "VMWare USB3 xHCI 0.96 Controller"; }
+                case 0x0779: { return "VMWare USB3 xHCI 1.00 Controller"; }
+                case 0x0790: { return "VMWare PCI Bridge"; }
+                case 0x07A0: { return "VMWare PCI Express Root Port"; }
+                case 0x07B0: { return "VMWare VMXNET3 Ethernet Controller"; }
+                case 0x07C0: { return "VMWare PVSCSI SCSI Controller"; }
+                case 0x07E0: { return "VMWare SATA AHCI Controller"; }
+                case 0x0801: { return "VMWare VM Interface"; }
+                case 0x0820: { return "VMWare Paravirtual RDMA Controller"; }
+                case 0x0800: { return "VMWare Hypervisor ROM Interface"; }
+                case 0x1977: { return "VMWare HD Audio Controller"; }
+                default: { return "Unrecognized VMWare Device"; }
+            }
+        }
+
+        // vmware
+        if (vendor == PCIVendors::VMWareInc)
+        {
+            switch (id)
+            {
+                default: { return "Unrecognized VMWare Device"; }
+            }
+        }
+
+        // innotek
+        if (vendor == PCIVendors::InnoTek)
+        {
+            switch (id)
+            {
+                case 0xBEEF: { return "VirtualBox Graphics Adapter"; }
+                case 0xCAFE: { return "VirtualBox Guest Service"; }
+                default: { return "Unrecognized InnoTek Device"; }
+            }
+        }
+
+        // amd
+        if (vendor == PCIVendors::AMD)
+        {
+            switch (id)
+            {
+                case 0x2000: { return "AMD 79C970 Ethernet Controller"; }
+            }
+        }
+
+        // qemu graphics controller
+        if (vendor == 0x1234 && id == 0x1111) { return "QEMU VGA Controller"; }
+
+        // unknown
+        return "Unrecognized Device";
     }
 }
