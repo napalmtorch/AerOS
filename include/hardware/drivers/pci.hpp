6428004e7c8a796df97325153064eea3df9f1c37
napalmtorch <napalmtorch@hotmail.com>
2021-05-05 16:04:29 -0400

    pci

---
+++ include/hardware/drivers/pci.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include "lib/types.h"
+#include "lib/string.hpp"
+#include "hardware/ports.hpp"
+
+namespace HAL
+{
+    struct _PCIDriver;
+
+    typedef struct
+    {
+        uint32_t Vendor;
+        uint32_t Device;
+        uint32_t Function;
+        struct _PCIDriver* Driver;
+    } PCIDevice;
+    
+    typedef struct
+    {
+        uint32_t Vendor;
+        uint32_t Device;
+        uint32_t Function;
+    } PCIDeviceID;
+    
+    typedef struct _PCIDriver
+    {
+        PCIDeviceID* Table;
+        char *Name;
+        uint8_t (*init_one)(PCIDevice*);
+        uint8_t (*init_driver)(void);
+        uint8_t (*exit_driver)(void);
+    } PCIDriver;
+
+    enum PCIVendors
+    {
+        Intel               = 0x8086,
+        IntelCorp           = 0x8087,
+        VMWare              = 0x15AD,
+        VMWareInc           = 0x0E0F,
+        Dell                = 0x1028,
+        DellCorporation     = 0x413C,
+        ATI                 = 0x1002,
+        AMD                 = 0x1022,
+        VIALabs             = 0x2109,
+        BenQ                = 0x04A5,
+        AcerInc             = 0x5986,
+        HPInc               = 0x03F0,
+        HPCompany           = 0x103C,
+        HPEnterprise        = 0x1590,
+        ASUS                = 0x1043,
+        ASUSInc             = 0x0B05,
+        InnoTek             = 0x80EE,
+        Realtek             = 0x10EC,
+        RealtekCorp         = 0x0BDA,
+    };
+
+    class PCIBusController
+    {
+        public:
+            void Initialize();
+            char* GetVendorName(uint16_t vendor, uint16_t id);
+        private:
+            void Probe();
+            void AddDevice(PCIDevice device);
+            uint16_t ReadWord(uint16_t bus, uint16_t slot, uint16_t func, uint16_t offset);
+    };
+}
---
+++ src/hardware/drivers/pci.cpp
@@ -0,0 +1,14 @@
+#include "hardware/drivers/pci.hpp"
+
+namespace HAL
+{
+    void PCIBusController::Initialize()
+    {
+
+    }
+
+    char* PCIBusController::GetVendorName(uint16_t vendor, uint16_t id)
+    {
+        
+    }
+}
