1e4d62b7e01c1267b635bd2e753f19eba4719e0d
Nicola Leone Ciardi <nicolaflavio22@gmail.com>
2021-05-07 02:39:03 +0200

    lil fix to terminal

--- src/core/kernel.cpp
+++ src/core/kernel.cpp
@@ -195,27 +195,30 @@ namespace System
         // triggered when enter key is pressed
         void KernelBase::OnEnterPressed(char* input)
         {
+            HandleCommand(input);
             Terminal.Write("shell> ",COL4_YELLOW);
-            //HandleCommand(input);
         }
 
         void KernelBase::HandleCommand(char* input)
         {
-                 
+            char* str = strsplit_index(input,0,' ');
             if(strcmp ("lspci", input) == 0) { PCIdevices.List(); }
             else if(strcmp ("clear", input) == 0) { Terminal.Clear(COL4_BLACK); Terminal.SetCursorPos(0,0); }
             else if(strcmp ("", input) == 0) { Terminal.WriteLine(""); }
-            else if(streql ("color",strsplit_index(input,0,' '))) { 
+            else if(streql ("color", str)) { 
                 
                 char* background = strsplit_index(input,2,' ');
                 char* foreground = strsplit_index(input,1,' ');
-                if(streql(strsplit_index(input,0,' '),foreground) || streql(strsplit_index(input,0,' '),background)) { Terminal.WriteLine("All Parameters are required"); Terminal.WriteLine("Usage: color foreground background"); Terminal.WriteLine("Example: color white black"); return; }//man let me test
+                if(streql(str,foreground) || streql(str,background)) { Terminal.WriteLine("All Parameters are required"); Terminal.WriteLine("Usage: color foreground background"); Terminal.WriteLine("Example: color white black"); return; }//man let me test
                 System::KernelIO::WriteLine(foreground);
                 System::KernelIO::WriteLine(background);
                 Terminal.Clear(Graphics::GetColorFromName(background));
                 Terminal.SetForeColor(Graphics::GetColorFromName(foreground));
                 Terminal.Write("shell> ",COL4_YELLOW);
                 Terminal.WriteLine("Color Set!");
+                delete background;
+                delete foreground;
+                delete str;
                 return;
             }
             else if (streql("debug", input)){
@@ -227,7 +230,7 @@ namespace System
             }
             else { Terminal.Write("You typed: ",COL4_WHITE); 
             Terminal.WriteLine(input); }
-            Terminal.Write("shell> ",COL4_YELLOW);
+            delete str;
             return;
         }
     }
