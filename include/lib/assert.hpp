55baad1bc23ad6f39be7756a4609e707f4fd7bb7
napalmtorch <napalmtorch@hotmail.com>
2021-05-08 16:40:46 -0400

    Mild assert name changes

--- include/lib/assert.hpp
+++ include/lib/assert.hpp
@@ -1,76 +1,80 @@
-// ------------------------------------------------------------------------------------------------
-// test/test.h
-// ------------------------------------------------------------------------------------------------
-
 #pragma once
-
 #include <lib/types.h>
-// ------------------------------------------------------------------------------------------------
 
-void AssertTrue(const char *expr, bool result, const char *file, unsigned line);
-void AssertEqChar(const char *expr, char result, char expected, const char *file, unsigned line);
-void AssertEqHex(const char *expr, unsigned short result, unsigned short expected, unsigned width, const char *file, unsigned line);
-void AssertEqMem(const char *expr, void *result, void *expected, size_t len, const char *file, unsigned line);
-void AssertEqPtr(const char *expr, void *result, void *expected, const char *file, unsigned line);
-void AssertEqStr(const char *expr, const char *result, const char *expected, const char *file, unsigned line);
-void AssertEqInt(const char *expr, long long result, long long expected, const char *file, unsigned line);
-void AssertEqUint(const char *expr, unsigned long long result, unsigned long long expected, const char *file, unsigned line);
+void assert_true(const char *expr, bool result, const char *file, unsigned line);
+void assert_eql_char(const char *expr, char result, char expected, const char *file, unsigned line);
+void assert_eql_hex(const char *expr, unsigned short result, unsigned short expected, unsigned width, const char *file, unsigned line);
+void assert_eql_mem(const char *expr, void *result, void *expected, size_t len, const char *file, unsigned line);
+void assert_eql_ptr(const char *expr, void *result, void *expected, const char *file, unsigned line);
+void assert_eql_str(const char *expr, const char *result, const char *expected, const char *file, unsigned line);
+void assert_eql_int(const char *expr, long long result, long long expected, const char *file, unsigned line);
+void assert_eql_uint(const char *expr, unsigned long long result, unsigned long long expected, const char *file, unsigned line);
 
-#define ASSERT_TRUE(expr)       AssertTrue(#expr, expr, __FILE__, __LINE__)
-
-#define ASSERT_EQ_CHAR(x, y)    AssertEqChar(#x, x, y, __FILE__, __LINE__)
-#define ASSERT_EQ_HEX8(x, y)    AssertEqHex(#x, x, y, 2, __FILE__, __LINE__)
-#define ASSERT_EQ_HEX16(x, y)   AssertEqHex(#x, x, y, 4, __FILE__, __LINE__)
-#define ASSERT_EQ_HEX32(x, y)   AssertEqHex(#x, x, y, 8, __FILE__, __LINE__)
-#define ASSERT_EQ_HEX64(x, y)   AssertEqHex(#x, x, y, 16, __FILE__, __LINE__)
-#define ASSERT_EQ_PTR(x, y)     AssertEqPtr(#x, x, y, __FILE__, __LINE__)
-#define ASSERT_EQ_STR(x, y)     AssertEqStr(#x, x, y, __FILE__, __LINE__)
-#define ASSERT_EQ_INT(x, y)     AssertEqInt(#x, x, y, __FILE__, __LINE__)
-#define ASSERT_EQ_UINT(x, y)    AssertEqUint(#x, x, y, __FILE__, __LINE__)
+#define ASSERT_TRUE(expr)       assert_true(#expr, expr, __FILE__, __LINE__)
+#define ASSERT_EQ_CHAR(x, y)    assert_eql_char(#x, x, y, __FILE__, __LINE__)
+#define ASSERT_EQ_HEX8(x, y)    assert_eql_hex(#x, x, y, 2, __FILE__, __LINE__)
+#define ASSERT_EQ_HEX16(x, y)   assert_eql_hex(#x, x, y, 4, __FILE__, __LINE__)
+#define ASSERT_EQ_HEX32(x, y)   assert_eql_hex(#x, x, y, 8, __FILE__, __LINE__)
+#define ASSERT_EQ_HEX64(x, y)   assert_eql_hex(#x, x, y, 16, __FILE__, __LINE__)
+#define ASSERT_EQ_PTR(x, y)     assert_eql_ptr(#x, x, y, __FILE__, __LINE__)
+#define ASSERT_EQ_STR(x, y)     assert_eql_str(#x, x, y, __FILE__, __LINE__)
+#define ASSERT_EQ_INT(x, y)     assert_eql_int(#x, x, y, __FILE__, __LINE__)
+#define ASSERT_EQ_UINT(x, y)    assert_eql_uint(#x, x, y, __FILE__, __LINE__)
 
 // ------------------------------------------------------------------------------------------------
 
-typedef struct ExpectedValue
+namespace Assert
 {
-    struct ExpectedValue *next;
-    union
+    // expected value structure
+    typedef struct ExpectedValue
     {
-        char ch;
-        int n;
-    } u;
-} ExpectedValue;
+        struct ExpectedValue *next;
+        union { char ch; int n; } u;
+    } ExpectedValue;
 
-typedef struct ExpectedQueue
-{
-    struct ExpectedQueue *next;
-    const char *function;
-    const char *param;
-    ExpectedValue *head;
-    ExpectedValue **tail;
-} ExpectedQueue;
+    // expected queue structure
+    typedef struct ExpectedQueue
+    {
+        struct ExpectedQueue *next;
+        const char *function;
+        const char *param;
+        ExpectedValue *head;
+        ExpectedValue **tail;
+    } ExpectedQueue;
 
-void InitQueue(ExpectedQueue *q, const char *function, const char *param);
-void MockFailure();
-void MatchInt(ExpectedQueue *q, int n);
-void MatchChar(ExpectedQueue *q, char ch);
-void ExpectChar(ExpectedQueue *q, char ch);
-void ExpectInt(ExpectedQueue *q, int n);
-void EndQueue(ExpectedQueue *q);
-void PreMock(const char *file, unsigned line);
-void PostMock();
+    void InitializeQueue(ExpectedQueue *q, const char *function, const char *param);
+    void MockFailure();
+    void MatchInt(ExpectedQueue *q, int n);
+    void MatchChar(ExpectedQueue *q, char ch);
+    void ExpectChar(ExpectedQueue *q, char ch);
+    void ExpectInt(ExpectedQueue *q, int n);
+    void EndQueue(ExpectedQueue *q);
+    void PreMock(const char *file, unsigned line);
+    void PostMock();
 
-#define MATCH_CHAR(function, param) MatchChar(&Mock.function##param, param)
-#define MATCH_INT(function, param) MatchInt(&Mock.function##param, param)
+    void IsTrue(const char *expr, bool result, const char *file, unsigned line);
+    void EqualsChar(const char *expr, char result, char expected, const char *file, unsigned line);
+    void EqualsHex(const char *expr, unsigned short result, unsigned short expected, unsigned width, const char *file, unsigned line);
+    void EqualsMem(const char *expr, void *result, void *expected, size_t len, const char *file, unsigned line);
+    void EqualsPointer(const char *expr, void *result, void *expected, const char *file, unsigned line);
+    void EqualsString(const char *expr, const char *result, const char *expected, const char *file, unsigned line);
+    void EqualsInteger(const char *expr, long long result, long long expected, const char *file, unsigned line);
+    void EqualsUInteger(const char *expr, unsigned long long result, unsigned long long expected, const char *file, unsigned line);
 
-#define EXPECT_CHAR(function, param) ExpectChar(&Mock.function##param, param)
-#define EXPECT_INT(function, param) ExpectInt(&Mock.function##param, param)
+    #define MATCH_CHAR(function, param) MatchChar(&Mock.function##param, param)
+    #define MATCH_INT(function, param) MatchInt(&Mock.function##param, param)
 
-#define DECLARE_QUEUE(function, param) ExpectedQueue function##param
+    #define EXPECT_CHAR(function, param) ExpectChar(&Mock.function##param, param)
+    #define EXPECT_INT(function, param) ExpectInt(&Mock.function##param, param)
 
-#define INIT_QUEUE(function, param) InitQueue(&Mock.function##param, #function, #param)
+    #define DECLARE_QUEUE(function, param) ExpectedQueue function##param
 
-#define MOCK_BEGIN PreMock(__FILE__, __LINE__)
-#define MOCK_END PostMock()
+    #define INIT_QUEUE(function, param) InitQueue(&Mock.function##param, #function, #param)
 
-#define RUN_MOCK(x)  MOCK_BEGIN; x; MOCK_END
-#define EXPECT(x) expect_##x
+    #define MOCK_BEGIN PreMock(__FILE__, __LINE__)
+    #define MOCK_END PostMock()
+
+    #define RUN_MOCK(x)  MOCK_BEGIN; x; MOCK_END
+    #define EXPECT(x) expect_##x
+
+}
--- src/lib/assert.cpp
+++ src/lib/assert.cpp
@@ -1,17 +1,8 @@
-// ------------------------------------------------------------------------------------------------
-// test/test.c
-// ------------------------------------------------------------------------------------------------
-
 #include <lib/assert.hpp>
 #include <hardware/memory.hpp>
 
-// ------------------------------------------------------------------------------------------------
-static ExpectedQueue *s_params;
-static const char *s_file;
-static unsigned s_line;
-
-// ------------------------------------------------------------------------------------------------
-void AssertTrue(const char *expr, bool result, const char *file, unsigned line)
+// assert trye
+void assert_true(const char *expr, bool result, const char *file, unsigned line)
 {
     System::KernelIO::Write("Testing: ");
     System::KernelIO::WriteLine((char*)file);
@@ -23,8 +14,8 @@ void AssertTrue(const char *expr, bool result, const char *file, unsigned line)
     return;
 }
 
-// ------------------------------------------------------------------------------------------------
-void AssertEqChar(const char *expr, char result, char expected, const char *file, unsigned line)
+// assert equals char
+void assert_eql_char(const char *expr, char result, char expected, const char *file, unsigned line)
 {
     System::KernelIO::Write("Testing: ");
     System::KernelIO::WriteLine((char*)file);
@@ -36,19 +27,19 @@ void AssertEqChar(const char *expr, char result, char expected, const char *file
     return;
 }
 
-// ------------------------------------------------------------------------------------------------
-void AssertEqHex(const char *expr, unsigned short result, unsigned short expected, unsigned width, const char *file, unsigned line)
+// assert equals hex
+void assert_eql_hex(const char *expr, unsigned short result, unsigned short expected, unsigned width, const char *file, unsigned line)
 {
     if (result != expected)
     {
         System::KernelIO::WriteLine("Test AssertEqHex failed");
     }
-      System::KernelIO::WriteLine("Test AssertEqHex Succeeded");
-      return;
+    System::KernelIO::WriteLine("Test AssertEqHex Succeeded");
+    return;
 }
 
-// ------------------------------------------------------------------------------------------------
-void AssertEqMem(const char *expr, void *result, void *expected, size_t len, const char *file, unsigned line)
+// assert equals mem
+void assert_eql_mem(const char *expr, void *result, void *expected, size_t len, const char *file, unsigned line)
 {
     if (memcmp(result, expected, len))
     {
@@ -58,150 +49,165 @@ void AssertEqMem(const char *expr, void *result, void *expected, size_t len, con
         for (size_t i = 0; i < len; ++i)
         {
         }
-          System::KernelIO::WriteLine("Test memcmp Succeeded");
-       return;
+        System::KernelIO::WriteLine("Test memcmp Succeeded");
+    return;
     }
 }
 
-// ------------------------------------------------------------------------------------------------
-void AssertEqPtr(const char *expr, void *result, void *expected, const char *file, unsigned line)
+// assert equals pointer
+void assert_eql_ptr(const char *expr, void *result, void *expected, const char *file, unsigned line)
 {
     System::KernelIO::Write("Testing: ");
     System::KernelIO::WriteLine((char*)file);
     if (result != expected)
     {
-          System::KernelIO::WriteLine("Test AssertEqPtr failed");
+        System::KernelIO::WriteLine("Test AssertEqPtr failed");
     }
     System::KernelIO::WriteLine("Test AssertEqPtr Succeeded");
     return;
 }
 
-// ------------------------------------------------------------------------------------------------
-void AssertEqStr(const char *expr, const char *result, const char *expected, const char *file, unsigned line)
+// assert equals string
+void assert_eql_str(const char *expr, const char *result, const char *expected, const char *file, unsigned line)
 {
     if (strcmp((char*)result, (char*)expected))
     {
     }
 }
 
-// ------------------------------------------------------------------------------------------------
-void AssertEqInt(const char *expr, long long result, long long expected, const char *file, unsigned line)
+// assert equals integer
+void assert_eql_int(const char *expr, long long result, long long expected, const char *file, unsigned line)
 {
     if (result != expected)
     {
     }
 }
 
-// ------------------------------------------------------------------------------------------------
-void AssertEqUint(const char *expr, unsigned long long result, unsigned long long expected, const char *file, unsigned line)
+// assert equals unsigned integer
+void assert_eql_uint(const char *expr, unsigned long long result, unsigned long long expected, const char *file, unsigned line)
 {
     if (result != expected)
     {
     }
 }
 
-
-// ------------------------------------------------------------------------------------------------
-void InitQueue(ExpectedQueue *q, const char *function, const char *param)
+namespace Assert
 {
-    q->next = s_params;
-    q->function = function;
-    q->param = param;
-    q->head = 0;
-    q->tail = &q->head;
-
-    s_params = q;
-}
-
-// ------------------------------------------------------------------------------------------------
-void MockFailure()
-{
-
-}
-
-// ------------------------------------------------------------------------------------------------
-void MatchInt(ExpectedQueue *q, int n)
-{
-    if (q->head == 0)
+    static ExpectedQueue *s_params;
+    static const char *s_file;
+    static unsigned s_line;
+    // ------------------------------------------------------------------------------------------------
+    void InitializeQueue(ExpectedQueue *q, const char *function, const char *param)
     {
-        MockFailure();
+        q->next = s_params;
+        q->function = function;
+        q->param = param;
+        q->head = 0;
+        q->tail = &q->head;
+
+        s_params = q;
     }
-    else
-    {
-        ExpectedValue *v = q->head;
-        q->head = v->next;
 
-        if (v->u.n != n)
+    // ------------------------------------------------------------------------------------------------
+    void MockFailure()
+    {
+
+    }
+
+    // ------------------------------------------------------------------------------------------------
+    void MatchInt(ExpectedQueue *q, int n)
+    {
+        if (q->head == 0)
         {
             MockFailure();
         }
-    }
-}
+        else
+        {
+            ExpectedValue *v = q->head;
+            q->head = v->next;
 
-// ------------------------------------------------------------------------------------------------
-void MatchChar(ExpectedQueue *q, char ch)
-{
-    if (q->head == 0)
-    {
-        MockFailure();
+            if (v->u.n != n)
+            {
+                MockFailure();
+            }
+        }
     }
-    else
-    {
-        ExpectedValue *v = q->head;
-        q->head = v->next;
 
-        if (v->u.ch != ch)
+    // ------------------------------------------------------------------------------------------------
+    void MatchChar(ExpectedQueue *q, char ch)
+    {
+        if (q->head == 0)
+        {
+            MockFailure();
+        }
+        else
+        {
+            ExpectedValue *v = q->head;
+            q->head = v->next;
+
+            if (v->u.ch != ch)
+            {
+                MockFailure();
+            }
+
+            mem_free(v);
+        }
+    }
+
+    // ------------------------------------------------------------------------------------------------
+    void ExpectChar(ExpectedQueue *q, char ch)
+    {
+        ExpectedValue *v = (ExpectedValue *)mem_alloc(sizeof(ExpectedValue));
+        v->next = 0;
+        v->u.ch = ch;
+        *q->tail = v;
+        q->tail = &v;
+    }
+
+    // ------------------------------------------------------------------------------------------------
+    void ExpectInt(ExpectedQueue *q, int n)
+    {
+        ExpectedValue *v = (ExpectedValue *)mem_alloc(sizeof(ExpectedValue));
+        v->next = 0;
+        v->u.n = n;
+        *q->tail = v;
+        q->tail = &v;
+    }
+
+    // ------------------------------------------------------------------------------------------------
+    void EndQueue(ExpectedQueue *q)
+    {
+        if (q->head)
         {
             MockFailure();
         }
 
-        mem_free(v);
+        q->tail = &q->head;
     }
-}
 
-// ------------------------------------------------------------------------------------------------
-void ExpectChar(ExpectedQueue *q, char ch)
-{
-    ExpectedValue *v = (ExpectedValue *)mem_alloc(sizeof(ExpectedValue));
-    v->next = 0;
-    v->u.ch = ch;
-    *q->tail = v;
-    q->tail = &v;
-}
-
-// ------------------------------------------------------------------------------------------------
-void ExpectInt(ExpectedQueue *q, int n)
-{
-    ExpectedValue *v = (ExpectedValue *)mem_alloc(sizeof(ExpectedValue));
-    v->next = 0;
-    v->u.n = n;
-    *q->tail = v;
-    q->tail = &v;
-}
-
-// ------------------------------------------------------------------------------------------------
-void EndQueue(ExpectedQueue *q)
-{
-    if (q->head)
+    // ------------------------------------------------------------------------------------------------
+    void PreMock(const char *file, unsigned line)
     {
-        MockFailure();
+        s_file = file;
+        s_line = line;
     }
 
-    q->tail = &q->head;
-}
-
-// ------------------------------------------------------------------------------------------------
-void PreMock(const char *file, unsigned line)
-{
-    s_file = file;
-    s_line = line;
-}
-
-// ------------------------------------------------------------------------------------------------
-void PostMock()
-{
-    for (ExpectedQueue *q = s_params; q; q = q->next)
+    // ------------------------------------------------------------------------------------------------
+    void PostMock()
     {
-        EndQueue(q);
+        for (ExpectedQueue *q = s_params; q; q = q->next)
+        {
+            EndQueue(q);
+        }
     }
+
+    void IsTrue(const char *expr, bool result, const char *file, unsigned line) { assert_true(expr, result, file, line); }
+    void EqualsChar(const char *expr, char result, char expected, const char *file, unsigned line) { assert_eql_char(expr, result, expected, file, line); }
+    void EqualsHex(const char *expr, unsigned short result, unsigned short expected, unsigned width, const char *file, unsigned line) { assert_eql_hex(expr, result, expected, width, file, line); }
+    void EqualsMem(const char *expr, void *result, void *expected, size_t len, const char *file, unsigned line) { assert_eql_mem(expr, result, expected, len, file, line); }
+    void EqualsPointer(const char *expr, void *result, void *expected, const char *file, unsigned line) { assert_eql_ptr(expr, result, expected, file, line); }
+    void EqualsString(const char *expr, const char *result, const char *expected, const char *file, unsigned line) { assert_eql_str(expr, result, expected, file, line); }
+    void EqualsInteger(const char *expr, long long result, long long expected, const char *file, unsigned line) { assert_eql_int(expr, result, expected, file, line); }
+    void EqualsUInteger(const char *expr, unsigned long long result, unsigned long long expected, const char *file, unsigned line) { assert_eql_uint(expr, result, expected, file, line); }
+
 }
