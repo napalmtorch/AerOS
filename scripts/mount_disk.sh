9be9bc45ddbe8b343eb0b149ac7f5c8227c58a89
KM198912 <k.meerts@online.de>
2021-05-11 04:05:05 +0200

    script cleanup

--- scripts/mount_disk.sh
+++ scripts/mount_disk.sh
@@ -5,13 +5,13 @@ if [ "$EUID" -ne 0 ]
     exit
     else
 if [ "$1" == "mount" ]; then
-sudo mkdir /mnt/tmp
-sudo mount disks/fat32.img /mnt/tmp
+mkdir /mnt/tmp
+mount disks/fat32.img /mnt/tmp
 echo "Displaying Contents of /mnt/tmp"
 ls -al /mnt/tmp
 elif [ "$1" == "unmount" ]; then
-sudo umount /mnt/tmp
-sudo rm -Rf /mnt/tmp
+umount /mnt/tmp
+rm -Rf /mnt/tmp
 else
 echo "Provide 'mount' or 'unmount' as argument"
 fi
